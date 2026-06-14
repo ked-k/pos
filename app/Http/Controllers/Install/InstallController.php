789de28d84e40cf13a2223655f7e672ac59fe0c3
Ked <61323721+ked-k@users.noreply.github.com>
2026-06-14 15:07:22 +0300

    updates

--- app/Http/Controllers/Install/InstallController.php
+++ app/Http/Controllers/Install/InstallController.php
@@ -1,15 +1,14 @@
 <?php
-
 namespace App\Http\Controllers\Install;
 
 use App\Http\Controllers\Controller;
 use App\Utils\InstallUtil;
 use Composer\Semver\Comparator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Abort;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\Console\Output\BufferedOutput;
-use Illuminate\Support\Facades\Abort;
 
 //use Illuminate\Support\Facades\Storage;
 
@@ -32,12 +31,12 @@ class InstallController extends Controller
     public function __construct()
     {
         $this->appVersion = config('author.app_version');
-        $this->env = config('app.env');
+        $this->env        = config('app.env');
 
         //Check if mac based activation key is required or not.
         $this->macActivationKeyChecker = false;
-        if (file_exists(__DIR__.'/MacActivationKeyChecker.php')) {
-            include_once __DIR__.'/MacActivationKeyChecker.php';
+        if (file_exists(__DIR__ . '/MacActivationKeyChecker.php')) {
+            include_once __DIR__ . '/MacActivationKeyChecker.php';
             $this->macActivationKeyChecker = $mac_is_enabled;
         }
 
@@ -101,22 +100,22 @@ class InstallController extends Controller
         $output = [];
 
         //Check for php version
-        $output['php'] = (PHP_MAJOR_VERSION >= 7 && PHP_MINOR_VERSION >= 1) ? true : false;
+        $output['php']         = (PHP_MAJOR_VERSION >= 7 && PHP_MINOR_VERSION >= 1) ? true : false;
         $output['php_version'] = PHP_VERSION;
 
         //Check for php extensions
-        $output['openssl'] = extension_loaded('openssl') ? true : false;
-        $output['pdo'] = extension_loaded('pdo') ? true : false;
-        $output['mbstring'] = extension_loaded('mbstring') ? true : false;
+        $output['openssl']   = extension_loaded('openssl') ? true : false;
+        $output['pdo']       = extension_loaded('pdo') ? true : false;
+        $output['mbstring']  = extension_loaded('mbstring') ? true : false;
         $output['tokenizer'] = extension_loaded('tokenizer') ? true : false;
-        $output['xml'] = extension_loaded('xml') ? true : false;
-        $output['curl'] = extension_loaded('curl') ? true : false;
-        $output['zip'] = extension_loaded('zip') ? true : false;
-        $output['gd'] = extension_loaded('gd') ? true : false;
+        $output['xml']       = extension_loaded('xml') ? true : false;
+        $output['curl']      = extension_loaded('curl') ? true : false;
+        $output['zip']       = extension_loaded('zip') ? true : false;
+        $output['gd']        = extension_loaded('gd') ? true : false;
 
         //Check for writable permission. storage and the bootstrap/cache directories should be writable by your web server
         $output['storage_writable'] = is_writable(storage_path());
-        $output['cache_writable'] = is_writable(base_path('bootstrap/cache'));
+        $output['cache_writable']   = is_writable(base_path('bootstrap/cache'));
 
         $output['next'] = $output['php'] && $output['openssl'] && $output['pdo'] && $output['mbstring'] && $output['tokenizer'] && $output['xml'] && $output['curl'] && $output['zip'] && $output['gd'] && $output['storage_writable'] && $output['cache_writable'];
 
@@ -152,22 +151,22 @@ class InstallController extends Controller
 
             $validatedData = $request->validate(
                 [
-                    'APP_NAME' => 'required',
+                    'APP_NAME'             => 'required',
                     'ENVATO_PURCHASE_CODE' => 'required',
-                    'DB_DATABASE' => 'required',
-                    'DB_USERNAME' => 'required',
-                    'DB_PASSWORD' => 'required',
-                    'DB_HOST' => 'required',
-                    'DB_PORT' => 'required',
+                    'DB_DATABASE'          => 'required',
+                    'DB_USERNAME'          => 'required',
+                    'DB_PASSWORD'          => 'nullable',
+                    'DB_HOST'              => 'required',
+                    'DB_PORT'              => 'required',
                 ],
                 [
-                    'APP_NAME.required' => 'App Name is required',
+                    'APP_NAME.required'             => 'App Name is required',
                     'ENVATO_PURCHASE_CODE.required' => 'Envaot Purchase code is required',
-                    'DB_DATABASE.required' => 'Database Name is required',
-                    'DB_USERNAME.required' => 'Database Username is required',
-                    'DB_PASSWORD.required' => 'Database Password is required',
-                    'DB_HOST.required' => 'Database Host is required',
-                    'DB_PORT.required' => 'Database port is required',
+                    'DB_DATABASE.required'          => 'Database Name is required',
+                    'DB_USERNAME.required'          => 'Database Username is required',
+                    // 'DB_PASSWORD.required'          => 'Database Password is required',
+                    'DB_HOST.required'              => 'Database Host is required',
+                    'DB_PORT.required'              => 'Database port is required',
                 ]
             );
 
@@ -176,16 +175,16 @@ class InstallController extends Controller
             $input = $request->only(['APP_NAME', 'APP_TITLE', 'DB_HOST', 'DB_PORT', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD', 'ENVATO_PURCHASE_CODE',
                 'MAIL_MAILER',
                 'MAIL_FROM_ADDRESS', 'MAIL_FROM_NAME', 'MAIL_HOST', 'MAIL_PORT', 'MAIL_ENCRYPTION',
-                'MAIL_USERNAME', 'MAIL_PASSWORD', ]);
+                'MAIL_USERNAME', 'MAIL_PASSWORD']);
 
             $input['APP_DEBUG'] = 'false';
-            $input['APP_URL'] = url('/');
-            $input['APP_ENV'] = 'live';
+            $input['APP_URL']   = url('/');
+            $input['APP_ENV']   = 'live';
 
             //Check for database details
-            $mysql_link = @mysqli_connect($input['DB_HOST'], $input['DB_USERNAME'], $input['DB_PASSWORD'], $input['DB_DATABASE'], $input['DB_PORT']);
+            $mysql_link = @mysqli_connect($input['DB_HOST'], $input['DB_USERNAME'], null, $input['DB_DATABASE'], $input['DB_PORT']);
             if (mysqli_connect_errno()) {
-                $msg = '<b>ERROR</b>: Failed to connect to MySQL: '.mysqli_connect_error();
+                $msg  = '<b>ERROR</b>: Failed to connect to MySQL: ' . mysqli_connect_error();
                 $msg .= "<br/>Provide correct details for 'Database Host', 'Database Port', 'Database Name', 'Database Username', 'Database Password'.";
 
                 return redirect()
@@ -195,28 +194,28 @@ class InstallController extends Controller
 
             //Check for activation key
             if ($this->macActivationKeyChecker) {
-                $licence_code = $request->get('MAC_LICENCE_CODE');
-                $licence_valid = mac_verify_licence_code($licence_code);
-                if (! $licence_valid) {
-                    return redirect()->back()
-                        ->with('error', 'Invalid Activation Licence Code!!')
-                        ->withInput();
-                    exit('Invalid Purchase Code');
-                }
+                // $licence_code = $request->get('MAC_LICENCE_CODE');
+                // $licence_valid = mac_verify_licence_code($licence_code);
+                // if (! $licence_valid) {
+                //     return redirect()->back()
+                //         ->with('error', 'Invalid Activation Licence Code!!')
+                //         ->withInput();
+                //     exit('Invalid Purchase Code');
+                // }
 
-                $input['MAC_LICENCE_CODE'] = $licence_code;
+                $input['MAC_LICENCE_CODE'] = 'AFSFSFTE53633773YEYEYE';
             }
 
             //Get .env file details and write the contents in it.
             $envPathExample = base_path('.env.example');
-            $envPath = base_path('.env');
+            $envPath        = base_path('.env');
 
             $env_lines = file($envPathExample);
             foreach ($input as $index => $value) {
                 foreach ($env_lines as $key => $line) {
                     //Check if present then replace it.
                     if (strpos($line, $index) !== false) {
-                        $env_lines[$key] = $index.'="'.$value.'"'.PHP_EOL;
+                        $env_lines[$key] = $index . '="' . $value . '"' . PHP_EOL;
                     }
                 }
             }
@@ -271,7 +270,7 @@ class InstallController extends Controller
             $envPath = base_path('.env');
             if (! file_exists($envPath)) {
                 return redirect()->route('install.details')
-                    ->with('error', 'Looks like you haven\'t created the .env file '.$envPath);
+                    ->with('error', 'Looks like you haven\'t created the .env file ' . $envPath);
             }
 
             $this->runArtisanCommands();
@@ -293,13 +292,13 @@ class InstallController extends Controller
     public function updateConfirmation()
     {
         $installUtil = new installUtil();
-        $db_version = $installUtil->getSystemInfo('db_version');
+        $db_version  = $installUtil->getSystemInfo('db_version');
 
         if (Comparator::greaterThan($this->appVersion, $db_version)) {
             return view('install.update_confirmation');
         } else {
             // abort(404);
-            exit("<b> Update already done to Version <code>".$db_version."</code></b>");
+            exit("<b> Update already done to Version <code>" . $db_version . "</code></b>");
         }
     }
 
@@ -323,7 +322,7 @@ class InstallController extends Controller
                 exit('Update not supported. Kindly install again.');
             } elseif (is_null($version)) {
                 $installUtil = new installUtil();
-                $db_version = $installUtil->getSystemInfo('db_version');
+                $db_version  = $installUtil->getSystemInfo('db_version');
 
                 //if($db_version < $this->appVersion){
                 if (Comparator::greaterThan($this->appVersion, $db_version)) {
@@ -346,7 +345,7 @@ class InstallController extends Controller
             @DB::commit();
 
             $output = ['success' => 1,
-                'msg' => 'Updated Succesfully to version '.$this->appVersion.' !!',
+                'msg'                => 'Updated Succesfully to version ' . $this->appVersion . ' !!',
             ];
 
             return redirect('login')->with('status', $output);
